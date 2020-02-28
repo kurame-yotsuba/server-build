4aadbfe7f83a61271ff9fbc4a31e04e3ecbe0632
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-02-29 01:11:44 +0900

    fstab設定用のスクリプトの置き場所の変更

--- scripts/mount/init_fstab.sh
+++ scripts/mount/init_fstab.sh
@@ -4,6 +4,8 @@
 # マウントの設定
 #================================================
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 # ユーザ（僕）が付け加えたマウント情報のファイル
-fstab_file="res/fstab.user"
+fstab_file="$SCRIPT_DIR/fstab.user"
 cat $fstab_file >> /etc/fstab
