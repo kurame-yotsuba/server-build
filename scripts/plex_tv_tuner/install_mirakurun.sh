36175b4b25343b23d73d501ab2d3cf7d7bc27bd6
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-02-29 13:50:39 +0900

    ログ管理用パッケージの追加と、ymlファイルのファイル名ミス修正

--- scripts/plex_tv_tuner/install_mirakurun.sh
+++ scripts/plex_tv_tuner/install_mirakurun.sh
@@ -10,11 +10,14 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 # nvmとnodejsのインストール
 $SCRIPT_DIR/install_node_lts.sh
 
+. ~/.bashrc
+
 # Mirakurunのインストール
 npm install pm2 -g
 npm install mirakurun -g --unsafe --production
 npm install arib-b25-stream-test -g --unsafe
 
+# mirakurunユーザの作成
 $SCRIPT_DIR/make_mirakurun_user.sh
 
 # statusがonlineなら正常
@@ -22,7 +25,7 @@ pm2 status
 
 # tuners.yamlのコピー
 mv /usr/local/etc/mirakurun/tuners.yaml /usr/local/etc/mirakurun/tuners.yaml.original
-cat $SCRIPT_DIR/tuners.yaml > /usr/local/etc/mirakurun/tuners.yaml
+cat $SCRIPT_DIR/tuners.yml > /usr/local/etc/mirakurun/tuners.yml
 
 mirakurun restart
 
@@ -31,3 +34,15 @@ curl -X PUT "http://localhost:40772/api/config/channels/scan"
 mirakurun restart
 
 # 待つ
+
+# このコマンドで空のservicesが消えたらOK
+echo "rivarun --list | sed 's/},/},\n/g'" | su - mirakurun
+
+# 録画確認
+echo "rivarun --b25 --sid 1024 --ch GR/27 15 test2.ts" | su - mirakurun
+echo "rivarun --b25 --mirakurun localhost:40772 --sid 1024 --ch GR/27 15 test3.ts" | su - mirakurun
+
+# ログ管理用
+pm2 install pm2-logrotate
+
+cat $SCRIPT_DIR/mirakurun.log.conf > /etc/logrotate.d/mirakurun
