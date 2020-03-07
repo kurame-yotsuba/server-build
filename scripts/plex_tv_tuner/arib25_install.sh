73b2575feca0a73e8e7dbc9fde5fee94e83c61b0
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-03-07 21:16:09 +0900

    パッケージ依存関係に対する敗北

--- scripts/plex_tv_tuner/arib25_install.sh
+++ scripts/plex_tv_tuner/arib25_install.sh
@@ -6,6 +6,13 @@
 # ホストで行う必要はない
 #================================================
 
+# 必要なパッケージのインストール
+# 全部必要なのかは不明
+dnf install wget git unzip bzip2 gcc gcc-c++ make automake kernel-devel patch perl-ExtUtils-MakeMaker libtool openssl-devel boost-devel cmake -y
+
+dnf --enablerepo=PowerTools install autogen nkf -y
+dnf --enablerepo=PowerTools install pcsc-lite-devel -y
+
 # arib25のインストール
 cd ~/src
 git clone https://github.com/stz2012/libarib25.git
