0b787af7835ce4bac1b50dc7b19d55a49e391deb
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-03-07 20:21:59 +0900

    スクリプトの修正

--- exec_auth_add.sh
+++ exec_auth_add.sh
@@ -4,4 +4,4 @@ script_dir=$(cd $(dirname $0); pwd)
 target_dir=$script_dir/scripts
 
 # スクリプトへの実行権限付与
-find $target_dir -type f -name *.sh | xargs chmod 755
+find $target_dir -type f -name "*.sh" | xargs chmod 755
